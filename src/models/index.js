4cccdb151a40d393dedd5a6384eb704c6c77cf4b
Thông Thịnh <74982487+thongnv99@users.noreply.github.com>
2021-04-12 10:22:22 +0700

    fix db

--- src/models/index.js
+++ src/models/index.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql");
-const mariadb = require("mariadb");
-const con = await mariadb.createConnection({
+
+const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "123456",
   database: "landing-page",
 });
 
-// con.connect(async function (err) {
-//   if (err) throw err;
+con.connect(async function (err) {
+  if (err) throw err;
 
-//   console.log("Connected!");
-// });
+  console.log("Connected!");
+});
 
 module.exports = con;
